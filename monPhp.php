a6a149205701d337aa0bc3e6ce2151b2981529bd
remy60 <r.dumoulin@outlook.com>
2016-05-09 12:33:08 +0200

    exercice fini

---
+++ monPhp.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<body>
+
+          <?php
+   $chiffre1=5;
+  $chiffre2=6;
+  $resultat=$chiffre1 + $chiffre2;
+      echo $resultat;
+  $resultat2=$chiffre1 * $chiffre2;
+      echo $resultat2;
+          ?>
+
+	</body>
+</html>
